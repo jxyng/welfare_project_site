fdd150f42c76649dca8dee584692ccb9b15144a2
batpak <haborang3141592@gmail.com>
2026-08-03 19:56:11 +0900

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="border-t mt-10">
+      <div className="container py-6 text-center text-sm text-gray-500">
+        © 2026 Welfare Finder
+      </div>
+    </footer>
+  );
+}
