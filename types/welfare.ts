909f1ffae5ba3973bfb6e799bf969092060a29c8
batpak <haborang3141592@gmail.com>
2026-08-03 19:55:49 +0900

    Create welfare.ts

---
+++ types/welfare.ts
@@ -0,0 +1,13 @@
+export interface Welfare {
+  id: string;
+  title: string;
+  description: string;
+  region: string;
+  age: string;
+  income: string;
+  category: string;
+  target: string;
+  deadline: string;
+  organization: string;
+  link: string;
+}
